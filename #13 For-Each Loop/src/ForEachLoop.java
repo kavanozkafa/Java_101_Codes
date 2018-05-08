adc5604051d1431fad9f15f148e2e0ea8f5fa8fe
Şammas Çölkesen <sammascolkesen@gmail.com>
2018-05-08 19:33:24 +0300

    Add files via upload

---
+++ #13 For-Each Loop/src/ForEachLoop.java
@@ -0,0 +1,19 @@
+public class ForEachLoop {
+    public static void main (String args[]) {
+
+        String names[]={"şammas","ayşe","mesud"};
+
+        //foreach
+        for (String i:names) {
+            System.out.println(i);
+
+        }
+
+
+        for (int i=0;i<3;i++){
+            System.out.println(names[i]);
+
+        }
+
+    }
+}
