56ed0691c6e2c490bebae71a3a50e260450d4c5f
Şammas Çölkesen <sammascolkesen@gmail.com>
2018-05-08 18:42:57 +0300

    Add files via upload

---
+++ #11 Do-While Loop/src/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main (String args[]) {
+
+        int i=0;
+    do {
+       i= i*2;
+        System.out.println("i'nin değeri" + i);
+        i++;
+
+    }while (i>10);
+
+    }
+}
